027f7be4648ce27ef6c7d697d1f057b3cc3f0605
jonca79 <jonas.almlof@igp.uu.se>
2026-04-09 11:25:31 +0200

    fix: R handling of quote characters

--- workflow/scripts/fragmentomics_metrics/filter_comm_panels.R
+++ workflow/scripts/fragmentomics_metrics/filter_comm_panels.R
@@ -5,7 +5,19 @@ output_file <- snakemake@output[[1]]
 
 # Load reads
 reads <- read_tsv(input_file, show_col_types = FALSE, quote = "",
-         col_names = c("chr", "start", "stop", "ens_id", "refseq", "gene", "exon", "strand"))
+         col_names = c("chr", "start", "stop", "ens_id", "refseq", "gene", "exon", "strand"),
+         col_types = cols(
+             chr = col_character(),
+             start = col_double(),
+             stop = col_double(),
+             ens_id = col_character(),
+             refseq = col_character(),
+             gene = col_character(),
+             exon = col_character(),
+             strand = col_character()
+         ))
+
+reads <- reads %>% filter(!is.na(start) & !is.na(stop) & start < stop)
 
 if (ncol(reads) < 8) {
     stop(paste("Input file", input_file, "has fewer than 8 columns. Data may be malformed."))
