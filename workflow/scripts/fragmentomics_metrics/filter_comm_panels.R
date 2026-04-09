1c8155c976b9b8f8ceb03d10fcb0d41fb95a1637
jonca79 <jonas.almlof@igp.uu.se>
2026-04-09 10:10:49 +0200

    fix: R handling of quote characters

--- workflow/scripts/fragmentomics_metrics/filter_comm_panels.R
+++ workflow/scripts/fragmentomics_metrics/filter_comm_panels.R
@@ -4,7 +4,7 @@ input_file <- snakemake@input[[1]]
 output_file <- snakemake@output[[1]]
 
 # Load reads
-reads <- read_tsv(input_file, show_col_types = F,
+reads <- read_tsv(input_file, show_col_types = FALSE, quote = "",
          col_names = c("chr", "start", "stop", "ens_id", "refseq", "gene", "exon", "strand"))
 
 if (ncol(reads) < 8) {
@@ -16,4 +16,4 @@ reads <- reads %>% select(chr, start, stop, ens_id, refseq, gene, exon, strand)
 # Output to target path
 output_dir <- dirname(output_file)
 dir.create(output_dir, showWarnings = FALSE, recursive = TRUE)
-write_tsv(reads, output_file, col_names = FALSE)
+write_tsv(reads, output_file, col_names = FALSE, quote = "none")
